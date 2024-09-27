e49646cc6053500e4226e161ca1d6a445e0d7099
ParvusDomus <parvusdomus@gmail.com>
2024-09-27 21:07:00 +0200

    fix apply damage when out of combat

--- modules/chat.js
+++ modules/chat.js
@@ -8,23 +8,43 @@ export default class mdiChat {
       const element = event.currentTarget;
       let target= Array.from(game.user.targets)[0]?.actor;
       const effect = CONFIG.statusEffects.find(e => e.id === CONFIG.specialStatusEffects.DEFEATED);
-      const combatant = game.combat.getCombatantByActor(target);
+      let combatant 
+      if (game.combat){
+        combatant= game.combat.getCombatantByActor(target);
+      }
       if (!target){
         ui.notifications.warn(game.i18n.localize("Debes seleccionar un objetivo"));
       }
       else {
-        let currentpv=Number(target.system.pv.value)
-        currentpv-=Number(dataset.dano)
+        let currentpv=Number(target.system.pv.value)-Number(dataset.dano)
         if (currentpv < 0){currentpv=0}
         target.update ({ 'system.pv.value': currentpv });
         ui.notifications.info(target.name+" recibe daño: "+dataset.dano); 
         if (currentpv <= 0){
           ui.notifications.info(target.name+" ha muerto...");
           target.update({defeated: true});
-          combatant.token.toggleActiveEffect(effect, {overlay: true});
-          return combatant.update({defeated: true});
-          
+          if (combatant){
+            combatant.token.toggleActiveEffect(effect, {overlay: true});
+            return combatant.update({defeated: true});
+          }
+          else{ 
+            let label = "dead";
+            let icon = "systems/mdi/style/icons/death-skull.svg"
+            let effect = {
+              label,
+              icon,
+              flags: {
+                core:{
+                  statusId: "dead",
+                  overlay: true
+                }
+              }
+            }
+            target.createEmbeddedDocuments("ActiveEffect", [effect])
+          }
         }
+
+        console.log (target)
       }
 
     }
